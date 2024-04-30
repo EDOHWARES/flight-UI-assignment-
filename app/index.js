b4a522757471143cd2d8e376365c1bff55b09489
EDOHWARES <edohemmanuel4real@gmail.com>
2024-04-30 05:10:17 +0100

    activated the reset button

--- app/index.js
+++ app/index.js
@@ -1,8 +1,14 @@
 const body = document.querySelector('.icontainer');
 const cancelBtn = document.querySelector('.cancel-btn');
 const resetPage = document.querySelector('.reset');
+const resetBtn = document.querySelector('.reset button');
 
 cancelBtn.addEventListener('click', ()=> {
     body.style.display = 'none';
     resetPage.style.display = 'flex';
+});
+
+resetBtn.addEventListener('click', () => {
+    resetPage.style.display = 'none';
+    body.style.display = 'flex';
 });
